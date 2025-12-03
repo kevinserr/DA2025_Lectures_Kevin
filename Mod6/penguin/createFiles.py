8a1f0f5b16d0b8d499e9e07678838e20c3ca3d03
Kevin Serrano <kevinserrl2002@gmail.com>
2025-12-03 11:53:46 -0500

    uploaded penguin project

---
+++ Mod6/penguin/createFiles.py
@@ -0,0 +1,47 @@
+import seaborn as sns
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+penguins = sns.load_dataset('penguins')
+cols = ['bill_length_mm','flipper_length_mm', 'species']
+df = penguins[cols].dropna()
+
+X =df[['bill_length_mm', 'flipper_length_mm']]
+y = df['species']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.20, random_state=42, stratify=y
+)
+
+pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('clf', LogisticRegression(max_iter=1000))
+])
+
+pipe.fit(X_train, y_train)
+
+y_pred = pipe.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Test Accuracy (Train - Test): {acc:.3f}")
+joblib.dump(pipe, 'penguin_model.pkl')
+
+meta = {
+    'bill_length_mm': (float(X_train.bill_length_mm.min()),
+                       float(X_train.bill_length_mm.max())),
+    'flipper_length_mm':(float(X_train.flipper_length_mm.min()),
+                       float(X_train.flipper_length_mm.max())),
+    'bill_length_default': float(X_train.bill_length_mm.mean()),
+    'flipper_length_default': float(X_train.flipper_length_mm.mean()),
+    'classes': pipe.classes_.tolist(),
+    'test_accuracy': float(acc)                    
+}
+
+pd.Series(meta).to_json('penguin_meta.json')
+
+print('Saved penguin_model.pkl and penguin_meta.json')
